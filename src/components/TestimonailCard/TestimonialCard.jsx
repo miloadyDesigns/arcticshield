8800b882436463351c848e73ad05380c8a2f25bb
miloadyDesigns <miloadydesignsocial@gmail.com>
2024-12-11 23:03:25 +0500

    resolving bugs

--- src/components/TestimonailCard/TestimonialCard.jsx
+++ src/components/TestimonailCard/TestimonialCard.jsx
@@ -3,7 +3,7 @@ import "./style.css"
 const TestimonialCard = () => {
     return (
         <figure className="snip1157">
-            <blockquote>Calvin: You know sometimes when I'm talking, my words can't keep up with my thoughts... I wonder why we think faster than we speak. Hobbes: Probably so we can think twice.
+            <blockquote>Calvin: You know sometimes when I am talking, my words cannot keep up with my thoughts... I wonder why we think faster than we speak. Hobbes: Probably so we can think twice.
                 <div className="arrow"></div>
             </blockquote>
             <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample3.jpg" alt="sq-sample3" />
